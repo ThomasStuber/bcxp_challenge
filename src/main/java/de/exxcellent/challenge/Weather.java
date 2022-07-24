689cf5c2655618fab5a6a4ee5fde83326fac16db
ThomasStuber <thomasstuber@web.de>
2022-07-24 15:12:20 +0200

    created new class Weather including method to analyse the data

---
+++ src/main/java/de/exxcellent/challenge/Weather.java
@@ -0,0 +1,14 @@
+package de.exxcellent.challenge;
+
+public class Weather {
+
+	public static String analyseData(String file) {
+		
+		// read file
+		// calculate smallest difference
+		// return result
+		
+		return "";
+	}
+	
+}
